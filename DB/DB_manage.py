b5318095b928920b28c88e9e6d6ea5a2281c29c2
Ivan <ivanu1706@gmail.com>
2024-01-29 00:47:35 +1100

    Основа БД готова

--- DB/DB_manage.py
+++ DB/DB_manage.py
@@ -3,10 +3,39 @@ import sqlite3
 connect = sqlite3.connect('project_db.db')
 cur = connect.cursor()
 
+"""ТАБЛИЦА 'ПОЛЬЗОВАТЕЛИ' """
+# cur.execute('CREATE TABLE IF NOT EXISTS users ('
+#             'id INTEGER PRIMARY KEY,'
+#             'fio TEXT NOT NULL,'
+#             'grade TEXT NOT NULL,'
+#             'email TEXT NOT NULL,'
+#             'phone_numb TEXT NOT NULL,'
+#             'password TEXT NOT NULL)')
 
+"""ТАБЛИЦА 'АДМИНЫ' """
+# cur.execute('CREATE TABLE IF NOT EXISTS admins ('
+#             'id INTEGER PRIMARY KEY,'
+#             'fio INTEGER NOT NULL,'
+#             'email TEXT NOT NULL,'
+#             'password TEST NOT NULL)')
 
+"""ТАБЛИЦА 'КНИГИ' """
+# cur.execute('CREATE TABLE IF NOT EXISTS books ('
+#             'id INTEGER PRIMARY KEY,'
+#             'name TEXT NOT NULL,'
+#             'author TEXT NOT NULL,'
+#             'amount INTEGER NOT NULL)')
 
+"""ТАБЛИЦА 'КНИГА-ПОЛЬЗОВАТЕЛЬ' """
+# cur.execute('CREATE TABLE IF NOT EXIST CONNECTION ('
+#             'id INTEGER PRIMARY KEY,'
+#             'user_id INTEGER,'
+#             'book_id INTEGER,'
+#             'date_start '
+#             ''
+#             ')')
 
+cur.execute('SELECT ')
 
 connect.commit()
 connect.close()
